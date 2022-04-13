4770627adcd7c2a967f5e39334144be0607053a2
Olle Jörlid <a21olljo@student.his.se>
2022-04-13 15:10:08 +0200

    Added a setOnClick listener that displays a toast if the button is pressed

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.create_user);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onButtonClick();
+            }
+        });
+    }
+
+    private void onButtonClick() {
+        Toast.makeText(this, "Your profile has been created", Toast.LENGTH_LONG).show();
     }
 }
